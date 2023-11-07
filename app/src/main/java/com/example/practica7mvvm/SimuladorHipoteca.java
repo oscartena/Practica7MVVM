9597469b52cca18e942b132d5d9f34079694293a
oscartena <oscar.tena.marin@gmail.com>
2023-11-07 17:38:11 +0100

    Initial commit

---
+++ app/src/main/java/com/example/practica7mvvm/SimuladorHipoteca.java
@@ -0,0 +1,53 @@
+package com.example.practica7mvvm;
+
+public class SimuladorHipoteca {
+
+    public static class Solicitud {
+        public double capital;
+        public int plazo;
+
+        public Solicitud(double capital, int plazo) {
+            this.capital = capital;
+            this.plazo = plazo;
+        }
+    }
+
+    interface Callback {
+        void cuandoEsteCalculadaLaCuota(double cuota);
+        void cuandoHayaErrorDeCapitalInferiorAlMinimo(double capitalMinimo);
+        void cuandoHayaErrorDePlazoInferiorAlMinimo(int plazoMinimo);
+        void cuandoEmpieceElCalculo();
+        void cuandoFinaliceElCalculo();
+    }
+
+    public void calcular(Solicitud solicitud, Callback callback) {
+        double interes = 0;
+        double capitalMinimo = 0;
+        int plazoMinimo = 0;
+        try {
+            Thread.sleep(10000);   // simular operacion de larga duracion (10s)
+            interes = 0.01605;
+            capitalMinimo = 1000;
+            plazoMinimo = 2;
+        } catch (InterruptedException e) {}
+
+        callback.cuandoEmpieceElCalculo();
+
+        boolean error = false;
+        if (solicitud.capital < capitalMinimo) {
+            callback.cuandoHayaErrorDeCapitalInferiorAlMinimo(capitalMinimo);
+            error = true;
+        }
+
+        if (solicitud.plazo < plazoMinimo) {
+            callback.cuandoHayaErrorDePlazoInferiorAlMinimo(plazoMinimo);
+            error = true;
+        }
+
+        if(!error) {
+            callback.cuandoEsteCalculadaLaCuota(solicitud.capital * interes / 12 / (1 - Math.pow(1 + (interes / 12), -solicitud.plazo * 12)));
+        }
+
+        callback.cuandoFinaliceElCalculo();
+    }
+}
